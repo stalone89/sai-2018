27c87ddd378b84ee5cf71ff42dfe7a0c1d872e59
ekspek <daniel.de.schiffart@gmail.com>
2018-10-18 13:22:33 +0100

    Converted waypoint arrays to structs

--- src/lab1.c
+++ src/lab1.c
@@ -4,63 +4,71 @@
 #define EARTH_RADIUS 6378000
 #define M_PI 3.14159265358979323846
 
-double waypoint_dist(double waypoint1[], double waypoint2[]){
-	/* Accepts waypoints with format {lat, lon, alt} in radians, radians and meters, respectively. */
+/* Waypoint structure, with latitude and longitude in radians and altitude in meters. */
+struct waypoint {
+	double latitude;
+	double longitude;
+	int altitude;
+};
+
+double waypoint_dist(struct waypoint waypoint1, struct waypoint waypoint2){
+	/* Accepts a waypoint struct. */
 	/* Returns the distance between waypoints in meters. */
 	
-	double ortho = acos(cos(waypoint2[0]) * cos(waypoint1[1] - waypoint2[1]) * cos(waypoint1[0]) + sin(waypoint2[0]) * sin(waypoint1[0]));
+	double ortho = acos(cos(waypoint2.latitude) * cos(waypoint1.longitude - waypoint2.longitude) * cos(waypoint1.latitude) + sin(waypoint2.latitude) * sin(waypoint1.latitude));
 	
 	/* I'll keep these lines here just in case. */
-	/* bearing = atan2(waypoint2[1] - waypoint1[1], sigma(waypoint2[0]) - sigma(waypoint1[0])); */
-	/* dist = EARTH_RADIUS * fabs(waypoint2[0] - waypoint1[0]) * (1/cos(bearing)); */
+	/* bearing = atan2(waypoint2.longitude - waypoint1.longitude, sigma(waypoint2.latitude) - sigma(waypoint1.latitude)); */
+	/* dist = EARTH_RADIUS * fabs(waypoint2.latitude - waypoint1.latitude) * (1/cos(bearing)); */
 	
 	return ortho * EARTH_RADIUS;
 }
 
-double waypoint_fromdist(double waypoint1[], double dist, double heading){
-	/* Accepts a waypoint with format {lat, lon, alt} in radians, radians and meters, respectively, and also a distance in meters and a heading in radians. */
-	/* Currently returns only latitude as a function can't return multiple values (or arrays), waiting on struct to return. */
+struct waypoint waypoint_fromdist(struct waypoint waypoint1, double dist, double heading){
+	/* Accepts a waypoint struct, a distance in meters and a heading in radians. */
+	/* Returns a waypoint struct. */
 	
-	double waypoint2[3];
+	struct waypoint waypoint2;
 	
-	waypoint2[0] = asin(sin(waypoint1[0]) * cos(dist / EARTH_RADIUS) + cos(waypoint1[0]) * sin(dist / EARTH_RADIUS) * cos(heading));
-	waypoint2[1] = waypoint1[1] + atan2(sin(heading) * sin(dist / EARTH_RADIUS) * cos(waypoint1[0]), cos(dist / EARTH_RADIUS) - sin(waypoint1[0]) * sin(waypoint2[0]));
+	waypoint2.latitude = asin(sin(waypoint1.latitude) * cos(dist / EARTH_RADIUS) + cos(waypoint1.latitude) * sin(dist / EARTH_RADIUS) * cos(heading));
+	waypoint2.longitude = waypoint1.longitude + atan2(sin(heading) * sin(dist / EARTH_RADIUS) * cos(waypoint1.latitude), cos(dist / EARTH_RADIUS) - sin(waypoint1.latitude) * sin(waypoint2.latitude));
 	
-	return waypoint2[0];
+	return waypoint2;
 }
 
-double waypoint_depheading(double waypoint1[], double waypoint2[]){
-	/* Accepts waypoints with format {lat, lon, alt} in radians, radians and meters, respectively. */
+double waypoint_depheading(struct waypoint waypoint1, struct waypoint waypoint2){
+	/* Accepts two waypoint structs. */
 	/* Returns departure heading (from north) in radians. */
 	
-	double depheading = atan2(-cos(waypoint2[0]) * sin(waypoint1[1] - waypoint2[1]), -cos(waypoint2[0]) * cos(waypoint1[1] - waypoint2[1]) * sin(waypoint1[0]) + sin(waypoint2[0]) * cos(waypoint1[0]));
+	double depheading = atan2(-cos(waypoint2.latitude) * sin(waypoint1.longitude - waypoint2.longitude), -cos(waypoint2.latitude) * cos(waypoint1.longitude - waypoint2.longitude) * sin(waypoint1.latitude) + sin(waypoint2.latitude) * cos(waypoint1.latitude));
 	
 	return depheading;
 }
 
-double waypoint_appheading(double waypoint1[], double waypoint2[]){
-	/* Accepts waypoints with format {lat, lon, alt} in radians, radians and meters, respectively. */
+double waypoint_appheading(struct waypoint waypoint1, struct waypoint waypoint2){
+	/* Accepts two waypoint structs. */
 	/* Returns approach heading (from north) in radians. */
 	
-	double appheading = atan2(-sin(waypoint1[1] - waypoint2[1]) * cos(waypoint1[0]), sin(waypoint2[0]) * cos(waypoint1[1] - waypoint2[1]) * cos(waypoint1[0]) - cos(waypoint2[0]) * sin(waypoint1[0]));
+	double appheading = atan2(-sin(waypoint1.longitude - waypoint2.longitude) * cos(waypoint1.latitude), sin(waypoint2.latitude) * cos(waypoint1.longitude - waypoint2.longitude) * cos(waypoint1.latitude) - cos(waypoint2.latitude) * sin(waypoint1.latitude));
 	
 	return appheading;
 }
 
 int main(void){
 	
-	double waypoint1[3] = {0.71122167,	-1.29096245,	0};
-	double waypoint2[3] = {0.899425759,	-0.00290946386,	0};
+	struct waypoint waypoint1, waypoint2;
+	waypoint1.latitude = 0.71122167; waypoint1.longitude = -1.29096245; waypoint1.altitude = 0;
+	waypoint2.latitude = 0.899425759; waypoint2.longitude = -0.00290946386; waypoint2.altitude = 0;
 	
 	double dist = waypoint_dist(waypoint1, waypoint2);
 	double time, tas, theading;
 	double depheading = waypoint_depheading(waypoint1, waypoint2);
 	double appheading = waypoint_appheading(waypoint1, waypoint2);
-	double waypoint2_calc = waypoint_fromdist(waypoint1, dist, depheading);
+	struct waypoint waypoint2_calc = waypoint_fromdist(waypoint1, dist, depheading);
 	
 	printf("The distance is %f meters.\n", dist);
 	printf("The departure heading is %f degrees and the arrival heading is %f degrees.\n", depheading * 180.0 / M_PI, appheading * 180.0/M_PI);
-	printf("Comparing to the original waypoint2, latitude %f and longitude %f,\nCalculating from distance and heading we get latitude %f and longitude %f.\n", waypoint2[0], waypoint2[1], waypoint2_calc, 0.0);
+	printf("Comparing to the original waypoint2, latitude %f and longitude %f,\nCalculating from distance and heading we get latitude %f and longitude %f.\n", waypoint2.latitude, waypoint2.longitude, waypoint2_calc.latitude, waypoint2_calc.longitude);
 	
 	return 0;
 }
